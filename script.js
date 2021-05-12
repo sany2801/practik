a1a67b70168fc866cf63b390e0eda95f0f772680
Александр <sasha82018201@gmail.com>
2021-05-12 17:21:07 +0300

    firstcommit

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = typeof("str");
+let number = typeof (3);
+let boolen = typeof (true);
+let name = typeof (null);
+let name1 
+let symbol = typeof (Symbol('f00'));
+let begint = typeof (456743653489765n);
+
+console.log(string);
+console.log(number);
+console.log(boolen);
+console.log(name);
+console.log(name1);
+console.log(symbol);
+console.log(begint);
+
+let hith = prompt('Сколько вам лет?');
+alert(hith);
+
+let firstNumber = prompt('Введите первое число')**2;
+let secondNumber = prompt('Введите второе число')**2;
+let result = firstNumber + secondNumber; 
+alert('Ваш ответ: ' + result);
